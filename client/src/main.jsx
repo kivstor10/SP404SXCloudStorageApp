679dbc6ca7a908596635ea9c21cec353d4e32214
kivstor10 <94065305+kivstor10@users.noreply.github.com>
2025-02-23 22:46:34 +0000

    Minor mobile view adjustments and AWS Cognito initilisation

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Amplify } from 'aws-amplify';
+import cognitoAuthConfig from './aws-exports'; 
+import './index.css';
+import App from './App.jsx';
+
+
+Amplify.configure(cognitoAuthConfig);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
