2bc82857a36e2959f0d0f0dab643ab6b6fbe24c0
kivstor10 <94065305+kivstor10@users.noreply.github.com>
2025-03-02 23:24:55 +0000

    Configuring login/register auth

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App.jsx';
 
 import { Amplify } from 'aws-amplify';
-import outputs from '../../amplify_outputs.json';
+// import outputs from '../../amplify_outputs.json';
+import outputs from './amplify_outputs.json';
 Amplify.configure(outputs);
 
 createRoot(document.getElementById('root')).render(
