1705b368355f72591badbd8c0c3e2c48a5aef1d3
kivstor10 <94065305+kivstor10@users.noreply.github.com>
2025-03-03 17:17:07 +0000

    debugging issue where client does not have access to user pool config

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,29 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { secret } from '@aws-amplify/backend';
 
 import { Amplify } from 'aws-amplify';
-// import outputs from '../../amplify_outputs.json';
-import outputs from './amplify_outputs.json';
-Amplify.configure(outputs);
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: secret('USER_POOL_ID'),
+      userPoolClientId:  secret('USER_POOL_CLIENT_ID'),
+      identityPoolId:  secret('IDENTITY_POOL_ID'),
+      loginWith: {
+        email: true,
+      },
+      signUpVerificationMethod: "code",
+      userAttributes: {
+        email: {
+          required: true,
+        },
+      },
+      allowGuestAccess: false,
+    },
+  },
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
